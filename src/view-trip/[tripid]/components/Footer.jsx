f670295320d803e71385606e9abc9a132dd96140
eisaexe <141444285+eisaexe@users.noreply.github.com>
2024-12-24 11:27:01 +0530

    Add files via upload

---
+++ src/view-trip/[tripid]/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='my-7 bg-gray-100 py-4'>
+      <h2 className='text-center text-gray-600 text-lg font-medium'>
+        Created by <span className='font-bold text-gray-700'>Md Eisa</span>
+        <span className='block text-sm text-gray-500 mt-1'>
+          AI Trip Planner using Genrative AI: <span className='text-indigo-600 font-semibold'>One Stop Solution</span>
+        </span>
+      </h2>
+    </div>
+  );
+}
+
+export default Footer;
