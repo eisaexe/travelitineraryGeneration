1bb71ca300cf6b120a6649c7b9865d7a7e070a75
eisaexe <141444285+eisaexe@users.noreply.github.com>
2025-02-26 02:39:59 +0530

    index.jsx

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -147,7 +147,7 @@ function CreateTrip() {
 
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
-      <h2 className="font-bold text-3xl">Tell us your preferences 🏕️🏝️</h2>
+      <h2 className="font-bold text-3xl">Tell us your preferences 🏝️</h2>
       <p className="mt-3 text-gray-500 text-xl">
         Just provide some basic information, and our trip planner will generate
         a customized itinerary based on your preferences.
